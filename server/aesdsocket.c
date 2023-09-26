d3d839a788861a4bebd6eac42b5df91b916f8117
Luis Yague <luis.yague@vdi-e.es>
2023-09-26 22:34:17 +0200

    fix

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]) {
   }
 
   memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
+  hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;  // use my IP
 
