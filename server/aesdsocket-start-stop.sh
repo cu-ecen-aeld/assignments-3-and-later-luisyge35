0eb497c400e06c4cb2aea10031910af26512aef2
Luis Yague <luis.yague@vdi-e.es>
2023-09-26 21:04:07 +0200

    fix

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+SERVER_ARGS="-d"
+
 case "$1" in
   start)
   echo "Starting server"
-  start-stop-daemon -S -n server -a /usr/bin/aesdsocket -d
+  start-stop-daemon -S -n server -a /usr/bin/aesdsocket -- $SERVER_ARGS
   ;;
   stop)
   echo "Stopping server"
